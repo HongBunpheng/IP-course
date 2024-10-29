a59fef49d8e9a7d17dfc03d5f8169d5901bf108b
HongBunpheng <hongbunpheng89@gmail.com>
2024-10-29 14:50:47 +0700

    TP01

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,97 @@
-<script setup>
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+<script>
+import compon1 from './components/compon1.vue';
+import compon2 from './components/compon2.vue';
+export default {
+  name: "App",
+  components: {
+    compon1,compon2
+  },
+  data() {
+    return {
+      categories: [
+        { image: "src/assets/cat-13 1.png",title: "Cake & Milk", items: 14 , bgColor: "#f2fce4"},
+        { title: "Peach", items: 17, image: "src/assets/cat-11 1.png", bgColor: "#fffceb" },
+        { title: "Organic Kiwi", items: 21, image: "src/assets/cat-12 1.png", bgColor: "#ecffec"  },
+        { title: "Red Apple", items: 68,image: "src/assets/cat-9 1.png", bgColor: "#feefea"  },
+        { title: "Snack", items: 34, image: "src/assets/cat-3 1.png", bgColor: "#fff3eb"  },
+        { title: "Black plum", items: 25,image: "src/assets/cat-4 1.png", bgColor: "#fff3ff"  },
+        { title: "Vegetables", items: 65, image: "src/assets/cat-1 4.png", bgColor: "#f2fce4" },
+        { title: "Headphone", items: 33, image: "src/assets/cat-15 1.png", bgColor: "#fffceb"  },
+        { title: "Cake & Milk", items: 54, image: "src/assets/cat-14 1.png", bgColor: "#f2fce4"  },
+        { title: "Orange", items: 63,image: "src/assets/cat-7 1.png", bgColor: "#fff3ff"  },
+      ],
+      promos: [
+        {
+          title: "Everyday Fresh & Clean with Our Products",
+          image: "src/assets/Cms-04 1.png",
+          bgColor: "#f0e8d5",
+          buttonColor: "#34a853",
+        },
+        {
+          title: "Make your Breakfast Healthy and Easy",
+          image: "src/assets/Cat-01 1.png",
+          bgColor: "#f3e8e8",
+          buttonColor: "#34a853",
+        },
+        {
+          title: "The best Organic Products Online",
+          image: "src/assets/Cms-03 1.png",
+          bgColor: "#e7eaf3",
+          buttonColor: "#f5cb0f",
+        },
+      ],
+    };
+  },
+};
 </script>
-
 <template>
-  "This is my first VueJs project"
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="app">
+    <h1>This is my first VueJs project</h1>
+    <div class="category-row">
+      <compon1
+        v-for="(category, index) in categories"
+        :key="index"
+        :title="category.title"
+        :items="category.items"
+        :image="category.image"
+        :bgColor="category.bgColor"
+      />
     </div>
-  </header> -->
-
-  <!-- <RouterView /> -->
+    <div class="promo-row">
+      <compon2
+        v-for="(promo, index) in promos"
+        :key="index"
+        :title="promo.title"
+        :image="promo.image"
+        :bgColor="promo.bgColor"
+        :buttonColor="promo.buttonColor"
+      />
+    </div>
+  </div>
 </template>
+<style>
+template{
+  position: relative;
+}
+.app {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  left:11%;
+  right: 10%;
+  top: 15%;
+}
+.category-row {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  margin-bottom: 10px;
+}
+.promo-row {
+  display: flex;
+  gap: 15px;
+}
+</style>
 
---
+++ src/components/compon2.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="promo-banner" :style="{ backgroundColor: bgColor }">
+    <div>
+    <h2>{{ title }}</h2>
+    <compon3 :bgColor="buttonColor"/>
+    </div>
+    <img :src="image" alt="promo image" class="promo-image" >
+  </div>
+</template>
+
+<script>
+import compon3 from './compon3.vue';
+export default {
+  name: "compon2",
+  props: {
+    title: String,
+    image: String,
+    bgColor: String,
+    buttonColor: { 
+      type: String,
+    }
+  },
+  components:{
+    compon3,
+  },
+};
+</script>
+
+<style scoped>
+.promo-banner {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  border-radius: 10px;
+  color: #333;
+  height: 250px;
+  width: 420px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+h2 {
+  font-size:21px;
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: rgb(46, 43, 65);
+}
+.promo-image {
+  margin-top: 50px;
+  width: 160px;
+  height: auto;
+}
+</style>
